ea82cfb1959f45c4a6ebc0fd0f88eb731ad571b7
Nikolay Vasylchyshyn <younghead@ukr.net>
2014-10-21 16:32:43 +0300

    Added segments size and binary size estimated by me into the commentary.

--- ch06/hello.c
+++ ch06/hello.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/*
+ * hello.c
+ *                          ls -l       size: text    data    bss    total
+ * simple hello world:       7131              877     256      8     1141
+ * uninitialized global:     7154              877     256   4032     5165
+ * initialized global:      11198              877    4280      8     5165
+ * uninitialized local:     11218              909    4280      8     5197
+ * initialized local:       11218              989    4280      8     5277
+ * -ggdb3:                  28838              989    4280      8     5277
+ * -O3 (w/o -ggdb3):        11218              909    4280      8     5197
+ */
+
 int global[1000] = { 1, 2, 3, 4, 5, 6 };
 
 int foo(void) {
