b5344b8b26e44c04d225ee73b4df1b540e582408
Nikolay Vasylchyshyn <younghead@ukr.net>
2014-10-27 19:35:05 +0200

    updated type-promotion.c

--- ch08/type-promotion.c
+++ ch08/type-promotion.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /* Check assembly output:
  * gcc -S type-promotion.c */
+
 int main(void) {
+	/* No type promotion from float to double for bar() */
+	int bar(float x);
 	char c1 = 1, c2 = 2;
 	float f1 = 1.0f, f2 = 2.0f;
 	long long llx = 123456789012345678;
@@ -15,10 +18,21 @@ int main(void) {
 	 * fadds	36(%esp)
 	 * fstps	40(%esp) */
 	printf("the sum of f1 and f2 is %f\n", f1); /* f1 again implicitly promoted to double */
+	foo(f1);
+	bar(f2);
 
 	printf("long long llx = %d\n", llx); /* should be taken only 4 bytes instead of 8 */
 	printf("long long llx = %lld\n", llx); /* should be taken all 8 bytes */
 
 	printf("sizeof('A') = %d\n", sizeof('A'));
 	printf("'A' = %c\n", 'A'); /* 'A' should be promoted to int */
+}
+
+/* type promotion */
+int foo(double x) {
+	return printf("x is %f\n", x);
+}
+
+int bar(float x) {
+	return printf("x is %f\n", x);
 }
